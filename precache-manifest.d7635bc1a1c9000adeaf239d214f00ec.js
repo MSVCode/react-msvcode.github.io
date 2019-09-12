c722f8309434b4e63b33b0f80d019e727c7ec7f4
Vincent <centralofmsv@gmail.com>
2019-09-12 14:41:51 +0700

    - Added simple homepage

---
+++ precache-manifest.d7635bc1a1c9000adeaf239d214f00ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98175a4b9196546b738c03928f90767f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dbd03b3b291deb7056b",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a4b7fcc3594b1b3b326f",
+    "url": "/static/css/main.6c0ac296.chunk.css"
+  },
+  {
+    "revision": "9dbd03b3b291deb7056b",
+    "url": "/static/js/2.fd922499.chunk.js"
+  },
+  {
+    "revision": "a4b7fcc3594b1b3b326f",
+    "url": "/static/js/main.4b4980e7.chunk.js"
+  },
+  {
+    "revision": "a6bffdef0ccdc6d59bff",
+    "url": "/static/js/runtime~main.6bed41ca.js"
+  },
+  {
+    "revision": "48e73fd820d0b0151d23084795a39ca8",
+    "url": "/static/media/vincent.48e73fd8.png"
+  }
+]);
